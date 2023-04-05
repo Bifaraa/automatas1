630e3ef719a5ac2376f0441585f9d84036ca0e63
bifara <106451721+Bifaraa@users.noreply.github.com>
2023-04-04 21:18:12 -0500

    despliegue

--- cliente/src/services/dot.js
+++ cliente/src/services/dot.js
@@ -1,5 +1,5 @@
 export const dotFetch = async () => {
-  const res = await fetch('http://localhost:3000/dot')
+  const res = await fetch('/dot')
   const dat = await res.json()
   return dat.data
 }
